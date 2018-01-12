1fee3655821cf52bb676f6c26fbcafa1849f2b7d
wolfep15 <patrick.wolfe@tufts.edu>
2018-01-11 22:16:09 -0500

    Improved score calculation to scale better

--- Webpage/index.js
+++ Webpage/index.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
 				}
 				else {
 					var intScore = parseInt(data[i].score);
-					var points = 100 - Math.abs(intScore - intGuess); 
+					var points = 100 - Math.trunc(10*Math.sqrt(Math.abs(intScore - intGuess))); 
 					if (i == 0) {
 						$("#points").text(points);
 					}
